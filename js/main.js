c153fd78ba2f590e34ddc4c9aebd3b899f84f97f
PM5000 <p.orara32@gmail.com>
2017-09-14 02:25:03 -0700

    created game logic u10

--- js/main.js
+++ js/main.js
@@ -1,7 +1,29 @@
 console.log("Up and running!");
 
 //(U8)Create an array cards. Strings should be "queen", "queen", "king", and "king"
-var cards = ["Queen", "Queen", "King", "King"];
+//i(U10)Delete the four strings from the cards array.
+//i(U10)In the cards array, create four objects, one for each card.
+var cards = [{
+    rank: "Queen",
+    suit: "Hearts",
+    cardImage: "images/queen-of-hearts.png"
+  },
+  {
+    rank: "Queen",
+    suit: "Diamonds",
+    cardImage: "images/queen-of-diamonds.png"
+  },
+  {
+    rank: "King",
+    suit: "Hearts",
+    cardImage: "images/king-of-hearts.png"
+  },
+  {
+    rank: "King",
+    suit: "Diamonds",
+    cardImage: "images/king-of-diamonds.png"
+  }
+];
 //(U8)Create an array cardsInPlay, & set its value to an empty array []
 var cardsInPlay = [];
 //iiii(U9) Right below the cardsInPlay array and right above the flipCard
@@ -20,14 +42,27 @@ checkForMatch = function() {
 //i(U9)The function should accept one parameter: cardId.
 //ii(U9)Within the flipCard function, add a console.log()
 //ii(U9)statement to display the card that was flipped over.
+//ii(U10)In the flipCard function, find this console.log
+//ii(U10)statement: console.log("User flipped " + cards[cardId]);
+//ii(U10)Update the portion cards[cardId]. this code is accessing the entire
+//ii(U10)first object in the cards array.
+//ii(U10)update this code so that we are accessing the rank property of this object
+//ii(U10)..Update this to cards[cardId].rank
 var flipCard = function(cardId) {
-  console.log("User Flipped" + cards[cardId]);
+  console.log("User Flipped" + cards[cardId].rank);
   // Add card to array of cards that are in play
-  cardsInPlay.push(cards[cardId]);
-
-  //Check to see if two cards have been played
+  //iii(U10)update the cards[cardId] portion of cardsInPlay.push(cards[cardId]);.
+  //iii(U10)You'll want to push the name of the card...
+  //iii(U10)("queen" or "king") to the cardsInPlay array.
+  //iii(U10)use the rank property like we did in the last step
+  cardsInPlay.push(cards[cardId].rank);
+  //iii(U10) Add two console.log() statements. One should log the cardImage
+  //iii(U10)and the other should log the suit for the flipped card.
+  console.log(cards[cardId].cardImage)
+  console.log(cards[cardId].suit)
+  //Leave or no? Check to see if two cards have been played
   if (cardsInPlay.length === 2) {
-    //If so, call the checkForMatch function
+    //Leave or no? If so, call the checkForMatch function
     checkForMatch();
   }
 };
@@ -58,4 +93,3 @@ flipCard(2);
 //???  alert("Sorry, try again.");
 //???}
 //???}
-//kk
